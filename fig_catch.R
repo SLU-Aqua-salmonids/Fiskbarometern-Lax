ced3759c4657b359c184102559ab1ff46605ff8c
Anders Kagervall <anders.kagervall@gmail.com>
2024-11-13 16:36:31 +0100

    Add new figure for total catches in the Baltic.

--- fig_catch.R
+++ fig_catch.R
@@ -5,15 +5,17 @@ library(ggplot2)
 library(writexl)
 library(ggaqua) # remotes::install_github("maxlindmark/ggaqua")
 ####
-## This script will prepare a figure with a time series of catches of salmon by Swedish fishers
+## This script will prepare a figures with a time series of catches of salmon by Swedish fishers
 ## both recreational and commercial in the Bailtc sea and rivers entering the Baltic sea.
 ## The data for freshwater is taken from (the excel-dump of) fishdb, the data for the Baltic
 ## sea is taken from the WGBAST catch database.
+##
+## Besides the created PNGs we also save excel-files with the aggregated data used
+## to create the figures. This can be useful if you want to extract some numbers
+## to refer to in the text.
 
 
-
-
-## You can chang the location of the files if you want to run on "private" copies of the data
+## You can change the location of the files if you want to run on "private" copies of the data
 Data_root_folder <- "//storage-dh.slu.se/restricted$/Lax/Data"
 WGBAST_folder <- "WGBAST_CatchDatabase"
 Fishdata_folder <- "fishdb"
@@ -25,6 +27,7 @@ Fishdata_database <- file.path(Data_root_folder, Fishdata_folder, "fishdata-late
 #first_year <- as.numeric(format(Sys.Date(), "%Y")) - 19
 first_year <- 2003
 
+#### Fig catches in Sweden all fisheries ----
 river_data <- read_xlsx(Fishdata_database, sheet = "river_sum")
 
 sal_ts_river <- river_data %>% filter(maf == "SAL", ## Salmon
@@ -62,8 +65,8 @@ landed_per_year <- landed_per_year_river %>%
 landed_wide  <- landed_per_year %>%
   pivot_wider(names_from=fcat, values_from=c(N))
 
-write_xlsx(landed_per_year, "catch_long.xlsx")
-write_xlsx(landed_wide, "catch_wide.xlsx")
+write_xlsx(landed_per_year, "catch_long_swe.xlsx")
+write_xlsx(landed_wide, "catch_wide_swe.xlsx")
 
 ## Fix things in data to make the figure look OK
 fig_data <- landed_per_year  %>%
@@ -71,13 +74,14 @@ fig_data <- landed_per_year  %>%
     fcat == "Brood" ~ "Avelsfiske",
     fcat == "Commercial" ~ "Yrkesfiske",
     fcat == "Recreational" ~ "Fritidsfiske",
-    TRUE ~ "Okänt fiske. FIXA!"))
+    .default = "Okänt fiske. FIXA!"))
 
 
-y_axis <- "Fångster (antal i tusental)"
-##################### FÖR FLERA SERIER stapeldiagram
+y_axis <- "Landningar (antal i tusental)"
 pal <- c("#56B4E9", "#009E73", "#F0E442", "#0072B2", "#E69F00", "#D55E00")
-fig_catch <- ggplot(fig_data, aes(x = year, y = N/1000, fill = fcat)) +
+
+##################### FÖR FLERA SERIER stapeldiagram
+fig_catch_swe <- ggplot(fig_data, aes(x = year, y = N/1000, fill = fcat)) +
   geom_bar(stat = "identity") +
   labs(x = "", y = y_axis) +
   scale_fill_manual(values = pal) +
@@ -90,8 +94,48 @@ fig_catch <- ggplot(fig_data, aes(x = year, y = N/1000, fill = fcat)) +
                              keyheight = 0.03,
                              default.unit = "inch")) +
   theme_aqua() +
+#  theme(legend.text = element_text(size = rel(1.2))) +
+#  theme(axis.title.y = element_text(size = rel(1.8))) +
   theme(legend.box.margin = margin(-15,-15,-15,-15))
 
 # spara figuren
-ggsave("Fig_catch.png", plot = fig_catch, dpi = 300, width = 8, height = 8, units = "cm")
+ggsave("Fig_catch_swe.png", plot = fig_catch_swe, dpi = 300, width = 8, height = 8, units = "cm")
 
+
+####  Fig total catches in the Balitc by Countries ----
+
+f_types <- c("BMS", "BROOD","COMM", "RECR")
+total_catch <- sea_data %>%
+  filter(SPECIES == "SAL", YEAR >= first_year) %>%
+  filter(F_TYPE %in% f_types) %>%
+  filter(!is.na(NUMB)) %>% # Remove line without NUMB, maybe the database should be fixed instead?
+  mutate(Country = case_when(
+    COUNTRY == "DK" ~ "Danmark",
+    COUNTRY == "LT" ~ "Lettland",
+    COUNTRY == "SE" ~ "Sverige",
+    COUNTRY == "FI" ~ "Finland",
+    COUNTRY == "PL" ~ "Polen",
+    .default = "Övriga")) %>%
+  group_by(YEAR, Country) %>%
+  summarise(Landings = round(sum(NUMB) / 1000), .groups = "drop")
+
+fig_catch_tot <- ggplot(total_catch, aes(x = YEAR, y = Landings, fill = Country)) +
+  geom_bar(stat = "identity") +
+  labs(x = "", y = y_axis) +
+  scale_fill_manual(values = pal) +
+  labs(x = NULL) +
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(expand = c(0,0), labels = scales::label_comma()) +
+  guides(fill = guide_legend(nrow = 2,
+                             title = "",
+                             keywidth = 0.1,
+                             keyheight = 0.03,
+                             default.unit = "inch")) +
+  theme_aqua() +
+#  theme(legend.text = element_text(size = rel(1.2))) +
+#  theme(axis.title.y = element_text(size = rel(1.8))) +
+  theme(legend.box.margin = margin(-15,-15,-15,-15))
+
+ggsave("Fig_catch_tot.png", plot = fig_catch_tot, dpi = 300, width = 8, height = 8, units = "cm")
+
+write_xlsx(total_catch, "catch_long_tot.xlsx")
